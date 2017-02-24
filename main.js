ae4510dc8f48d55c40b5b22cf25d4b58b1bbc716
A. Maxwell Woods <amaxwoods@outlook.com>
2017-02-24 07:08:01 -0500

    Add initial javascript with stub functions

---
+++ main.js
@@ -0,0 +1,73 @@
+var calc = {
+    expression: [],
+
+    run: function () {
+        var buttons = document.getElementsByClassName('button');
+        var expression = "";
+
+        for (var i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", calc.handleButton);
+        }
+    },
+
+    handleButton: function(event) {
+        var val = event.target.getAttribute("data-val");
+
+        if (val == "=") {
+            calc.evaluate(calc.expression);
+            console.log("equals pressed");
+        }
+
+        else if (val == "AC") {
+            calc.expression = [];
+            calc.clearScreen();
+
+            console.log("expression cleared");
+        }
+
+        else if (val == "CE") {
+            // CE means clear the display, but don't wipe out the expression
+            calc.clearScreen();
+            console.log("entry cleared");
+        }
+
+        else {
+
+            // A number was pressed
+            if (!isNaN(parseInt(val))) {
+                lastButton = calc.expression[calc.expression.length - 1];
+
+                if (!isNaN(parseInt(lastButton))) {
+                    calc.expression[calc.expression.length - 1] = lastButton + val;
+                }
+
+                else {
+                    calc.expression.push(val);
+                }
+
+                calc.updateScreen(calc.expression[calc.expression.length - 1]);
+            }
+
+            // A function button was pressed
+            else {
+                calc.expression.push(val);
+                calc.updateScreen(null);
+            }
+
+            console.log("pressed: " + calc.expression);
+        }
+    },
+
+    evaluate: function(pressed) {
+        // todo
+    },
+
+    clearScreen: function() {
+
+    },
+
+    updateScreen: function(contents) {
+    }
+}
+
+document.addEventListener('DOMContentLoaded', calc.run);
